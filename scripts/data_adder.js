4a1e99f47a354067b66cbef7893d630bbbefc62f
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2023-09-03 11:56:14 +0400

    more added

--- scripts/data_adder.js
+++ scripts/data_adder.js
@@ -4,3 +4,30 @@ let usersjson = fs.readFileSync("data/data.json","utf-8");
 let data = JSON.parse(usersjson);
 
 console.log(data)
+
+
+// {
+//     "name": "",
+//     "named": false,
+//     "image": "images/",
+//     "text_form": "",
+//     "chemicals_used": [],
+//     "needs": [],
+//     "products": [],
+//     "chapter": "Haloalkanes",
+//     "tags": []
+    
+// }
+
+// {
+//     "name": "Nucliophilic substitution reaction of RX",
+//     "named": false,
+//     "image": "images/",
+//     "text_form": "RX +",
+//     "chemicals_used": ["RX"],
+//     "needs": [],
+//     "products": [],
+//     "chapter": "Haloalkanes",
+//     "tags": ["Nucliophilic substitution","Nucliophile",""]
+    
+// }
